e968287233fc81dd538565aea87f813c856034dc
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-09-05 12:29:39 +1200

    Add task model schema

--- models/task.js
+++ models/task.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-const tokenSchema = new mongoose.Schema({
+const taskSchema = new mongoose.Schema({
   task: { type: String, required: true },
-  completed: { type: Boolean, default: true },
+  completed: { type: Boolean, default: false },
 })
 
-export default mongoose.models.Task || mongoose.model('Task', tokenSchema)
+export default mongoose.models.Task || mongoose.model('Task', taskSchema)
